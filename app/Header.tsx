f8264681860555d5ef128171f0b4663b3a92e013
Saba Zikaria <szikaria961@gmail.com>
2023-02-19 21:01:53 -0500

    finish setting up skeleton code for our example

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className="p-5 bg-black">
-      <Link href="/" className="px-2 py-1 bg-white text-black-500 rounded-lg">Landing Page</Link>
+    <header className="p-5 bg-cyan-900">
+      <Link href="/" className="px-2 py-1 bg-cyan-200 text-black-500 rounded-lg">Landing Page</Link>
+      <Link href="/imageCollection" className="px-2 py-1 bg-cyan-200 text-black-500 rounded-lg">Image Collection</Link>
     </header>
   )
 }
