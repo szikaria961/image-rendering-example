da0294bd1018e897473bc325b53eb37607ccdc68
Saba Zikaria <szikaria961@gmail.com>
2023-02-25 10:40:45 -0500

    using useEffect to render data to compare with server-side rendering

--- pages/fruits/index.js
+++ pages/fruits/index.js
@@ -1,4 +1,20 @@
+// import React, { useEffect, useState } from 'react';
+
+
 function  FruitsList({ fruits }) {
+  /* Before implementing serverside rendering
+  const [names, setNames] = useState([]);
+
+  useEffect(()=> {
+    const fetchNames = async () => {
+      const res = await fetch('https://random-word-api.herokuapp.com/all')
+      const data = await res.json();
+      setNames(data);
+    }
+    fetchNames();
+  }, [])
+  console.log(names);
+  */
   return (
     <>
       <h1>List of Fruits</h1>
